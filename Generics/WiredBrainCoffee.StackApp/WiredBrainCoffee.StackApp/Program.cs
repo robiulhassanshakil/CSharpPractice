f44dd45849de19febc595012880b2738e2cf4f9f
shakil <victorshakil9102@gmail.com>
2022-05-16 01:20:26 +0600

    whitout generics

---
+++ Generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,17 @@
+﻿stackDoubles();
+
+static void stackDoubles()
+{
+    var stack = new SimpleStack();
+
+    stack.Push(1.2);
+    stack.Push(3.7);
+    stack.Push(5.6);
+
+    while (stack.count > 0)
+    {
+        var item = stack.Pop();
+
+        Console.WriteLine(item);
+    }
+}
---
+++ Generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,13 @@
+﻿public class SimpleStack
+{
+    public readonly double[] _items;
+    public SimpleStack()
+    {
+        _items = new double[10];
+    }
+    public int curentIndex = -1;
+    public int count => curentIndex +1;
+    public void Push(double item) => _items[++curentIndex] = item;
+    public double Pop() => _items[curentIndex--];
+
+}
